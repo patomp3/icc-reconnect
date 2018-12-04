3bb3bbf5dc991a7052b881bd293103e9ec984b58
patomp3 <45219240+patomp3@users.noreply.github.com>
2018-12-04 09:48:40 +0700

    add new api

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:1.11 as builder
+WORKDIR /go/src/github.com/patomp3/icc-reconnect
+RUN go get -d -v github.com/gorilla/mux
+COPY .  .
+#COPY reconnect.go .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o icc-reconnect .
+
+FROM alpine:latest
+RUN apk --no-cache add ca-certificates
+WORKDIR /root/
+COPY --from=builder /go/src/github.com/patomp3/icc-reconnect .
+CMD ["./icc-reconnect"]
--- main.go
+++ main.go
@@ -15,6 +15,8 @@ func main() {
 
 	router := mux.NewRouter()
 	router.HandleFunc("/reconnect", reconnect).Methods("POST")
+	router.HandleFunc("/disconnect", disconnect).Methods("POST")
+	router.HandleFunc("/cancel", cancel).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
@@ -38,6 +40,54 @@ func reconnect(w http.ResponseWriter, r *http.Request) {
 	//call recon api
 	var res ReconResult
 	res = ReconnectProduct(req)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func disconnect(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	//Read Json Request
+	var req DisconRequest
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(req)
+
+	//call recon api
+	var res DisconResult
+	res = DisconnectProduct(req)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(res)
+}
+
+func cancel(w http.ResponseWriter, r *http.Request) {
+
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	//Read Json Request
+	var req CancelRequest
+	err = json.Unmarshal(body, &req)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println(req)
+
+	//call recon api
+	var res CancelResult
+	res = CancelProduct(req)
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(res)
