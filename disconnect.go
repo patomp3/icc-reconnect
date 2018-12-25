89ed2a6f9edf36346896d0ad1d8a0069dc8989b1
patomp3 <45219240+patomp3@users.noreply.github.com>
2018-12-25 14:55:50 +0700

    add config
    add config

--- disconnect.go
+++ disconnect.go
@@ -37,7 +37,7 @@ type DisconResult struct {
 	ProductID   int    `json:"ProductId"`
 }
 
-//ReconnectProduct for ...
+//DisconnectProduct for ...
 func DisconnectProduct(req DisconRequest) DisconResult {
 	var myReturn DisconResult
 
@@ -64,7 +64,7 @@ func DisconnectProduct(req DisconRequest) DisconResult {
 	//Call Rest API ICC Reconnect
 	//jsonData := map[string]string{"ThaiId": "3909800183384"}
 	jsonValue, _ := json.Marshal(req)
-	response, err := http.Post("http://172.22.203.68//ConvergenceIBSTVG/ICC/disconnectproduct", "application/json", bytes.NewBuffer(jsonValue))
+	response, err := http.Post("http://172.22.203.68/ConvergenceIBSTVG/ICC/disconnectproduct", "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
 		fmt.Printf("The HTTP request failed with error %s\n", err)
 	} else {
--- main.go
+++ main.go
@@ -6,19 +6,63 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/spf13/viper"
 )
+
+type config struct {
+	port          string
+	reconnecturl  string
+	cancelurl     string
+	disconnecturl string
+}
 
 func main() {
 	fmt.Println("Start collection service.....")
+
+	//env := os.Args[1]
+	var env string
+	var cfg config
+
+	// For no assign parameter env. using default to Test
+	if len(os.Args) > 1 {
+		env = os.Args[1]
+	} else {
+		env = "Test"
+	}
+
+	viper.SetConfigName("app")    // no need to include file extension
+	viper.AddConfigPath("config") // set the path of your config file
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("Config file not found..." + err.Error())
+	} else {
+		if env == "production" {
+			cfg.port = viper.GetString("production.port")
+			cfg.reconnecturl = viper.GetString("production.reconnecturl")
+			cfg.disconnecturl = viper.GetString("production.disconnecturl")
+			cfg.cancelurl = viper.GetString("production.cancelurl")
+		} else {
+			cfg.port = viper.GetString("development.port")
+			cfg.reconnecturl = viper.GetString("development.reconnecturl")
+			cfg.disconnecturl = viper.GetString("development.disconnecturl")
+			cfg.cancelurl = viper.GetString("development.cancelurl")
+		}
+		fmt.Println("Env=" + env)
+		fmt.Println("serverPort=" + cfg.port)
+		fmt.Println("reconnecturl=" + cfg.reconnecturl)
+		fmt.Println("disconnecturl=" + cfg.disconnecturl)
+		fmt.Println("cancelurl=" + cfg.cancelurl)
+	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/reconnect", reconnect).Methods("POST")
 	router.HandleFunc("/disconnect", disconnect).Methods("POST")
 	router.HandleFunc("/cancel", cancel).Methods("POST")
 
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe(":"+cfg.port, router))
 }
 
 func reconnect(w http.ResponseWriter, r *http.Request) {
